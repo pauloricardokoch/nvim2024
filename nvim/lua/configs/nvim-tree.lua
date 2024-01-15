0482868d41e11be750152fc6fc41f407e623a0c4
Paulo Koch <pauloricardokoch@gmail.com>
2024-01-14 21:30:16 -0300

    update

--- nvim/lua/configs/nvim-tree.lua
+++ nvim/lua/configs/nvim-tree.lua
@@ -5,14 +5,11 @@ vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
--- empty setup using defaults
-require("nvim-tree").setup()
-
--- OR setup with some options
 require("nvim-tree").setup({
     sort_by = "case_sensitive",
     view = {
         width = 30,
+        side = "right",
     },
     renderer = {
         group_empty = true,
