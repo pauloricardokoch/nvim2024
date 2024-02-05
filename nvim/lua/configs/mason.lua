6e13ec849abc9d7cc3af1a5e14698694a7d84c8a
Paulo Koch <paulo.koch@g.globo>
2024-02-05 01:20:34 -0300

    update

--- nvim/lua/configs/mason.lua
+++ nvim/lua/configs/mason.lua
@@ -1,7 +1,12 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
     ensure_installed = {
-                    "lua_ls",
-                    "rust_analyzer",
-                },
+        "lua_ls",
+        "rust_analyzer",
+        "gopls",
+        "golangci_lint_ls",
+        "terraformls",
+        "tflint",
+        "tsserver",
+    },
 })
