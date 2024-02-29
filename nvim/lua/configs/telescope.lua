7a93c5968a85bdaf66dd2299898c74bffe0b0e8b
Paulo Koch <paulo.koch@g.globo>
2024-02-29 19:12:32 -0300

    update

--- nvim/lua/configs/telescope.lua
+++ nvim/lua/configs/telescope.lua
@@ -3,6 +3,21 @@ local tel = require("telescope")
 local rf = require("telescope").load_extension("recent_files")
 local set = vim.keymap.set
 
+local function filenameFirst(_, path)
+    local tail = vim.fs.basename(path)
+    local parent = vim.fs.dirname(path)
+    if parent == "." then return tail end
+    return string.format("%s\t\t%s", tail, parent)
+end
+
+tel.setup {
+    pickers = {
+        find_files = {
+            path_display = filenameFirst
+        }
+    }
+}
+
 -- telescope
 set("n", "<leader>sf", builtin.find_files, {})
 set("n", "<leader>st", tel.extensions.live_grep_args.live_grep_args, {})
