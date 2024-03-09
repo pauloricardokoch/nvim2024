8c9d93e44de49fc64c166aa9403119f3bb7297ab
Paulo Koch <paulo.koch@g.globo>
2024-03-09 01:42:23 -0300

    update

--- nvim/lua/configs/theme.lua
+++ nvim/lua/configs/theme.lua
@@ -1,20 +1,20 @@
 local cmd = vim.cmd
 
 require("catppuccin").setup({
-    flavour = "mocha",              -- latte, frappe, macchiato, mocha
+    flavour = "mocha",             -- latte, frappe, macchiato, mocha
     transparent_background = true, -- disables setting the background color.
-    show_end_of_buffer = false,     -- shows the "~" characters after the end of buffers
-    term_colors = true,             -- sets terminal colors (e.g. `g:terminal_color_0`)
+    show_end_of_buffer = false,    -- shows the "~" characters after the end of buffers
+    term_colors = true,            -- sets terminal colors (e.g. `g:terminal_color_0`)
     dim_inactive = {
-        enabled = false,            -- dims the background color of inactive window
+        enabled = false,           -- dims the background color of inactive window
         shade = "dark",
-        percentage = 0.15,          -- percentage of the shade to apply to the inactive window
+        percentage = 0.15,         -- percentage of the shade to apply to the inactive window
     },
-    no_italic = false,              -- Force no italic
-    no_bold = false,                -- Force no bold
-    no_underline = false,           -- Force no underline
-    styles = {                      -- Handles the styles of general hi groups (see `:h highlight-args`):
-        comments = { "italic" },    -- Change the style of comments
+    no_italic = false,             -- Force no italic
+    no_bold = false,               -- Force no bold
+    no_underline = false,          -- Force no underline
+    styles = {                     -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = { "italic" },   -- Change the style of comments
         conditionals = { "italic" },
         loops = {},
         functions = {},
@@ -51,7 +51,7 @@ require("rose-pine").setup({
 })
 
 function SetTheme(t)
-    t = t or "rp"
+    t = t or "cat"
     if t == "rp" then
         cmd [[ colorscheme rose-pine ]]
         return
