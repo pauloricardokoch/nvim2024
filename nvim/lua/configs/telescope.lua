42d65efb77be9cfbaf57bf3cdef06022c404a34f
Paulo Koch <paulo.koch@g.globo>
2024-05-24 00:59:24 -0300

    update

--- nvim/lua/configs/telescope.lua
+++ nvim/lua/configs/telescope.lua
@@ -18,8 +18,11 @@ tel.setup {
     }
 }
 
+local function find_files()
+    return builtin.find_files(require('telescope.themes').get_ivy({}))
+end
 -- telescope
-set("n", "<leader>sf", builtin.find_files, {})
+set("n", "<leader>sf", find_files, {})
 set("n", "<leader>st", tel.extensions.live_grep_args.live_grep_args, {})
 set("n", "<leader>sb", builtin.buffers, {})
 set("n", "<leader>sp", builtin.pickers, {})
